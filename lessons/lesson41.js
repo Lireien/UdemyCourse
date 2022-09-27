a43e3af4b92bbca996bc7e81f09e3c5b3e6cc86c
Anastasia <lireien@gmail.com>
2022-09-27 17:41:12 +0300

    feat: first point solved

--- lessons/lesson41.js
+++ lessons/lesson41.js
@@ -52,45 +52,48 @@ console.log(0 || "" || 2 || undefined || true || falsе); //2
 // Но в нынешнем виде мы обнаружили, что после её запуска не только копия данных содержит новых официантов, но и основные данные! В restorantData сотрудник Alice исчезает и заменяется Mike! Необходимо найти причину и немедленно исправить, чтобы данные были разделены.
 
 const restorantData = {
-    menu: [
-      {
-          name: 'Salad Caesar',
-          price: '14$'
-      },
-      {
-          name: 'Pizza Diavola',
-          price: '9$'
-      },
-      {
-          name: 'Beefsteak',
-          price: '17$'
-      },
-      {
-          name: 'Napoleon',
-          price: '7$'
-      }
+  menu: [{
+      name: 'Salad Caesar',
+      price: '14$'
+    },
+    {
+      name: 'Pizza Diavola',
+      price: '9$'
+    },
+    {
+      name: 'Beefsteak',
+      price: '17$'
+    },
+    {
+      name: 'Napoleon',
+      price: '7$'
+    }
   ],
-  waitors: [
-      {name: 'Alice', age: 22}, {name: 'John', age: 24}
-  ],
+  waitors: [{
+    name: 'Alice',
+    age: 22
+  }, {
+    name: 'John',
+    age: 24
+  }],
   averageLunchPrice: '20$',
   openNow: true
 };
 
 function isOpen(prop) {
   let answer = '';
-  prop ? answer = 'Закрыто' : answer = 'Открыто';
+  prop ? answer = 'Открыто' : answer = 'Закрыто';
 
-  return anwser;
+  return answer;
 }
 
-console.log(isOpen(openNow))
+console.log(isOpen(restorantData.openNow));
 
 function isAverageLunchPriceTrue(fDish, sDish, average) {
   if (+fDish.price.slice(0, -1) + (sDish.price) < average) {
-      return 'Цена ниже средней';
+    return 'Цена ниже средней';
   } else {
-      return 'Цена выше средней';
+    return 'Цена выше средней';
   }
 }
 
@@ -99,6 +102,9 @@ console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1]
 function transferWaitors(data) {
   const copy = Object.assign({}, data);
 
-  copy.waitors[0] = {name: 'Mike', age: 32};
+  copy.waitors[0] = {
+    name: 'Mike',
+    age: 32
+  };
   return copy;
 }

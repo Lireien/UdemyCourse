fdf01db3355e1ca6059428487c404f8934539299
Anastasia <lireien@gmail.com>
2022-09-27 21:09:12 +0300

    feat: complete les43 getting and inserting els on a page

--- lessons/lesson42/js/script.js
+++ lessons/lesson42/js/script.js
@@ -29,3 +29,49 @@ hearts.forEach(item => {
 const oneHeart = document.querySelector('.heart');
 //Получаем 1ый элемент со страницы с этим селектором
 console.log(oneHeart);
+
+// box.style.backgroundColor = 'blue';
+// box.style.width = '500px';
+
+box.style.cssText = 'background-color: blue; width: 500px';
+
+btns[1].style.borderRadius = '100%';
+
+for (let i = 0; i < circles.length; i++) {
+  circles[i].style.backgroundColor = 'yellow';
+}
+
+hearts.forEach(item => {
+  item.style.backgroundColor = 'chartreuse';
+});
+
+const div = document.createElement('div');
+// const text = document.createTextNode('TEXT');
+
+
+const wrapper = document.querySelector('.wrapper');
+div.classList.add('black');
+// document.body.append(div);
+wrapper.append(div);
+//устаревшее:
+// wrapper.appendChild(div);
+
+// wrapper.prepend(div);
+
+// hearts[0].before(div);
+// hearts[0].after(div);
+//устаревшее:
+// wrapper.insertBefore(div, hearts[1]);
+
+// circles[0].remove();
+//устаревшее:
+// wrapper.removeChild(hearts[1]);
+
+// hearts[0].replaceWith(circles[0]);
+//устаревшее:
+// wrapper.replaceChild(circles[0], hearts[0]);
+
+div.innerHTML = '<h1>Hello World</h1>';
+// div.textContent = 'Hello';
+
+div.insertAdjacentHTML('afterend', '<h2>New text</h2>');

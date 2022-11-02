d1b3aa63b4d6eee1aff827da9487923ab60e5de5
Anastasia <lireien@gmail.com>
2022-11-02 17:17:59 +0300

    fix: rename class to avoid lint error

--- lessons/lesson75.js
+++ lessons/lesson75.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//ES5
+
 function User(name, id) {
   this.name = name;
   this.id = id;
@@ -12,6 +14,23 @@ function User(name, id) {
 User.prototype.exit = function(name) {
   console.log(`User ${this.name} has left the room`);
 };
+
+
+//ES6
+
+class UserES6 {
+  constructor(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+  }
+  hello() {
+    console.log(`Hello, I'm ${this.name}`);
+  }
+  exit() {
+    console.log(`User ${this.name} has left the room`);
+  }
+}
 
 const mary = new User('Mary', 42263);
 const alex = new User('Alex', 22357);
